92ebe63de0c2d2df6f3ccd1ade4ffd62c595948e
tulasi manikanta <tulasi.bec@gmail.com>
2020-08-02 22:43:51 -0700

    Simple Merge Sort Algorithm

--- src/main/java/com/practice/alg/sorting/MergeSort.java
+++ src/main/java/com/practice/alg/sorting/MergeSort.java
@@ -1,87 +1,55 @@
 package com.practice.alg.sorting;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class MergeSort {
-    // Merges two subarrays of arr[].
-    // First subarray is arr[l..m]
-    // Second subarray is arr[m+1..r]
-    void merge(int[] arr, int l, int m, int r)
-    {
-        // Find sizes of two sub arrays to be merged
-        int leftArrayLength = m - l + 1;
-        int rightArrayLength = r - m;
-
-        /* Create temp arrays */
-        int[] left = new int [leftArrayLength];
-        int[] right = new int [rightArrayLength];
-
-        /*Copy original left array to temp left array*/
-        for (int i=0; i<leftArrayLength; ++i)
-            left[i] = arr[l + i];
-
-        /*Copy original left array to temp left array*/
-        for (int j=0; j<rightArrayLength; ++j)
-            right[j] = arr[m + 1+ j];
-
-
-        /* Merge the temp arrays */
-
-        // Initial indexes of first and second subarrays
-        int i = 0, j = 0;
-
-        // Initial index of merged subarry array
-        int k = l;
-        while (i < leftArrayLength && j < rightArrayLength)
-        {
-            if (left[i] <= right[j])
-            {
-                arr[k] = left[i];
-                i++;
-            }
-            else
-            {
-                arr[k] = right[j];
-                j++;
-            }
-            k++;
-        }
-
-        /* Copy remaining elements of left[] if any */
-        while (i < leftArrayLength)
-        {
-            arr[k] = left[i];
-            i++;
-            k++;
-        }
-
-        /* Copy remaining elements of right[] if any */
-        while (j < rightArrayLength)
-        {
-            arr[k] = right[j];
-            j++;
-            k++;
-        }
-    }
-
-    void sort(int[] arr, int l, int r)
-    {
-        if (l < r)
-        {
-            int m = (l+r)/2;
-            sort(arr, l, m);
-            sort(arr , m+1, r);
-            merge(arr, l, m, r);
-        }
-    }
-
     public static void main(String args[])
     {
-        int arr[] = {12, 11, 13, 5, 6, 7};
-        Arrays.stream(arr).forEach(System.out::print);
+        int arr[] = {16, 11, 13, 5, 6, 7};
+        divide(arr, 0, arr.length-1);
+        System.out.println();
+        Arrays.stream(arr).boxed().forEach(System.out::print);
 
-        MergeSort sorter = new MergeSort();
-        sorter.sort(arr, 0, arr.length-1);
-        Arrays.stream(arr).forEach(System.out::print);
+    }
+    public static void divide(int[] src, int left, int right) {
+        if (left < right) {
+            int mid = (right + left) / 2;
+            divide(src, left, mid);
+            divide(src, mid+1, right);
+            mergeSort(src, left, mid, mid+1, right);
+        }
+    }
+
+    public static void mergeSort(int[] src, int left, int leftLength, int right, int rightLength) {
+        int[] tempArray = new int[rightLength-left+1];
+        int tempInsertPosition = 0;
+        int startingPos = left;
+
+        while(left <= leftLength && right <= rightLength) {
+           if (src[left] < src[right]) {
+               tempArray[tempInsertPosition] = src[left];
+               left++;
+           } else {
+               tempArray[tempInsertPosition] = src[right];
+               right++;
+           }
+            tempInsertPosition++;
+        }
+
+        while (left <= leftLength) {
+            tempArray[tempInsertPosition] = src[left];
+            tempInsertPosition++;
+            left++;
+        }
+
+        while (right <= rightLength) {
+            tempArray[tempInsertPosition] = src[right];
+            right++;
+        }
+
+        for (int i = 0; i < tempArray.length ; i++) {
+            src[startingPos + i] = tempArray[i];
+        }
     }
 }
